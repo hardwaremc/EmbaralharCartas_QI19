ce477dc69f678709aa75d8f4edeb9296a6475d11
hardwaremc <167475971+hardwaremc@users.noreply.github.com>
2024-05-09 22:56:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+naipes = ['Ouro', 'Copas', 'Espadas', 'Paus']
+numeros = ['Ás', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
+cartas = []
+
+#for para naipes
+for i in range(4):
+  #for para numeros
+  for u in range(13):
+    cartas.append(str(numeros[u])+" de "+naipes[i])
+
+quantidade = int(input("Digite a quantidade de cartas"))
+
+random.shuffle(cartas)
+for i in range(quantidade):
+  print(cartas[i])
+
